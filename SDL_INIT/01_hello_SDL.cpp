c1880248e535ecaee9ad907603ed2a0dcfe28220
lochie <lochie.denholm@carsales.com.au>
2023-06-01 16:45:02 +1000

    abstract main into individual methods, add media

--- SDL_INIT/01_hello_SDL.cpp
+++ SDL_INIT/01_hello_SDL.cpp
@@ -1,7 +1,3 @@
-/*This source code copyrighted by Lazy Foo' Productions 2004-2023
-and may not be redistributed without written permission.*/
-
-//Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
 
@@ -9,18 +5,63 @@ and may not be redistributed without written permission.*/
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+// GLOBALS (nb. Allowing this anti pattern due to this being a single file program)
+SDL_Window* window = NULL;
+SDL_Surface* screenBackground= NULL;
+SDL_Surface* ground_tiles = NULL;
+
+// Methods
+
+bool init()
+{
+	bool success = false;
+	if (!(SDL_Init(SDL_INIT_VIDEO) < 0))
+	{
+		window = SDL_CreateWindow("Rendering an img via CPU", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		success = true;
+		if (!(window == NULL))
+		{
+			screenBackground = SDL_GetWindowSurface(window);
+		}
+		else
+		{
+			printf("SDL Window was not created %s", SDL_GetError());
+			success = false;
+		}	
+	}
+	else
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+	}
+	
+	return success;
+}
+
+bool loadMedia()
+{
+	bool success = false;
+	char tile_path[] = "media/ground_tiles.bmp";
+	ground_tiles = SDL_LoadBMP(tile_path);
+	if (!(ground_tiles == NULL))
+	{
+		success = true;
+	}
+	else
+	{
+		printf("Unable to load image at path: %s", tile_path);
+	}
+	return success;
+}
+
+// Free media & Shutdown SDL
+void closeSDL();
+
 int main( int argc, char* args[] )
 {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-	
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
-
 	//Initialize SDL
 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
 	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		
 	}
 	else
 	{
