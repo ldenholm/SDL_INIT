44e3d8b541f9c8bf07bef0be345a65227e99612b
Loch <47731607+ldenholm@users.noreply.github.com>
2023-06-03 11:57:56 +1000

    Feature/add key events (#1)
    * Add enum, loadImage abstract method and key surfaces array
    
    * Upd readme location
    
    * Add surface load method
    
    * add key press assets and event handlers

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# :palm_tree: SDL_INIT Learning Simple Direct Media Layer :palm_tree:
+
+### This project serves as a playground for me to experiement in learning the SDL graphics API.
--- SDL_INIT/01_hello_SDL.cpp
+++ SDL_INIT/01_hello_SDL.cpp
@@ -1,14 +1,31 @@
 #include <SDL.h>
 #include <stdio.h>
+#include <string>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+// Enums
+enum KeyPressSurfaces
+{
+	KEY_PRESS_SURFACE_DEFAULT,
+	KEY_PRESS_SURFACE_UP,
+	KEY_PRESS_SURFACE_DOWN,
+	KEY_PRESS_SURFACE_LEFT,
+	KEY_PRESS_SURFACE_RIGHT,
+	KEY_PRESS_SURFACE_TOTAL
+};
+
 // GLOBALS (nb. Allowing this anti pattern due to this being a single file program)
+
 SDL_Window* window = NULL;
 SDL_Surface* screenSurface = NULL;
 SDL_Surface* ground_tiles = NULL;
+// Maps surfaces to key press events
+SDL_Surface* keyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
+// Current rendered image
+SDL_Surface* currentSurface = NULL;
 
 // Methods
 
@@ -37,28 +54,79 @@ bool init()
 	return success;
 }
 
-// Load bmp
+// Load Individual Image
+SDL_Surface* loadSurface(std::string path)
+{
+	SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
+	if (loadedSurface == NULL)
+	{
+		printf("Unable to load image at path: %s", SDL_GetError());
+	}
+	return loadedSurface;
+}
+
+// Load assets
 bool loadMedia()
 {
-	bool success = false;
-	char tile_path[] = "media/ground_tiles.bmp";
-	ground_tiles = SDL_LoadBMP(tile_path);
-	if (ground_tiles != NULL)
+	bool success = true;
+	std::string path = "media/press.bmp";
+
+	// Load Default
+	keyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] = loadSurface(path);
+	if (keyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] == NULL)
 	{
-		success = true;
+		success = false;
+		printf("Unable to load image: %s", path.c_str());
 	}
-	else
+
+	// KeyUp
+	path = "media/up.bmp";
+	keyPressSurfaces[KEY_PRESS_SURFACE_UP] = loadSurface(path);
+	if (keyPressSurfaces[KEY_PRESS_SURFACE_UP] == NULL)
 	{
-		printf("Unable to load image at path: %s", tile_path);
+		success = false;
+		printf("Unable to load image: %s", path.c_str());
 	}
+
+	// KeyDown
+	path = "media/down.bmp";
+	keyPressSurfaces[KEY_PRESS_SURFACE_DOWN] = loadSurface(path);
+	if (keyPressSurfaces[KEY_PRESS_SURFACE_DOWN] == NULL)
+	{
+		success = false;
+		printf("Unable to load image: %s", path.c_str());
+	}
+
+	// KeyLeft
+	path = "media/left.bmp";
+	keyPressSurfaces[KEY_PRESS_SURFACE_LEFT] = loadSurface(path);
+	if (keyPressSurfaces[KEY_PRESS_SURFACE_LEFT] == NULL)
+	{
+		success = false;
+		printf("Unable to load image: %s", path.c_str());
+	}
+
+	// KeyRight
+	path = "media/right.bmp";
+	keyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] = loadSurface(path);
+	if (keyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] == NULL)
+	{
+		success = false;
+		printf("Unable to load image: %s", path.c_str());
+	}
+
 	return success;
 }
 
 // Dalloc & Shutdown SDL
 void closeSDL()
 {
-	SDL_FreeSurface(ground_tiles);
-	ground_tiles = NULL;
+	
+	for (int i = 0; i < KEY_PRESS_SURFACE_TOTAL; ++i)
+	{
+		SDL_FreeSurface(keyPressSurfaces[i]);
+		keyPressSurfaces[i] = NULL;
+	}
 	
 	// this also destroys the screen surface
 	SDL_DestroyWindow(window);
@@ -66,6 +134,7 @@ void closeSDL()
 
 	SDL_Quit;
 }
+
 
 int main( int argc, char* args[] )
 {
@@ -85,6 +154,9 @@ int main( int argc, char* args[] )
 
 	SDL_Event e;
 
+	// Default surface
+	currentSurface = keyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+
 	// Game Loop:
 	while (!quit)
 	{
@@ -96,10 +168,35 @@ int main( int argc, char* args[] )
 			{
 				quit = true;
 			}
+			else if (e.type == SDL_KEYDOWN)
+			{
+				switch (e.key.keysym.sym)
+				{
+				case SDLK_UP:
+					currentSurface = keyPressSurfaces[KEY_PRESS_SURFACE_UP];
+					break;
+
+				case SDLK_DOWN:
+					currentSurface = keyPressSurfaces[KEY_PRESS_SURFACE_DOWN];
+					break;
+
+				case SDLK_LEFT:
+					currentSurface = keyPressSurfaces[KEY_PRESS_SURFACE_LEFT];
+					break;
+
+				case SDLK_RIGHT:
+					currentSurface = keyPressSurfaces[KEY_PRESS_SURFACE_RIGHT];
+					break;
+
+				default:
+					currentSurface = keyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+					break;
+				}
+			}
 		}
 
 		// Blit the tiles_surface(source) onto the screen_surface(target)
-		SDL_BlitSurface(ground_tiles, NULL, screenSurface, NULL);
+		SDL_BlitSurface(currentSurface, NULL, screenSurface, NULL);
 
 		// Update the window
 		SDL_UpdateWindowSurface(window);
