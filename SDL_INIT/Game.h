aeca1e99dea0dd384c16d2a91cf3b322757ed8a5
Loch <47731607+ldenholm@users.noreply.github.com>
2023-06-06 15:51:40 +1000

    Feature/pong (#6)
    * refactor wall creation
    
    * refactor walls again
    
    * draw paddle and ball and add delta time
    
    * Add left paddle movement
    
    * Simple pong

--- SDL_INIT/Game.h
+++ SDL_INIT/Game.h
@@ -1,4 +1,11 @@
 #include <SDL.h>
+
+struct Vector2
+{
+	float x;
+	float y;
+};
+
 class Game
 {
 public:
@@ -8,13 +15,20 @@ public:
 	void Shutdown();
 
 private:
+	bool mIsRunning;
 	SDL_Window* mWindow;
 	SDL_Renderer* mRenderer;
-	bool mIsRunning;
-	
+	Vector2 mPaddlePos;
+	Vector2 mBallPos;
+	Vector2 mBallVel;
+	Uint32 mTicksCount;
+	int mLPaddleDir;
+
 	// Helper functions for game loop:
 	void ProcessInput();
 	void UpdateGame();
 	void GenerateOutput();
+	void HandleLPaddleDirection(const Uint8* kbState);
+	void HandleLPaddleMovement(float deltaTime);
+	void HandleBallMovement(float deltaTime);
 };
-
